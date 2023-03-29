994406d5a22183b7e3410021d607e3aac28c042e
Ditidey <ditidey2017@gmail.com>
2023-03-29 14:17:52 +0100

    font awesome added

--- src/components/SingleMeal/SingleMeal.jsx
+++ src/components/SingleMeal/SingleMeal.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './SingleMeal.css'
+import './SingleMeal.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faCheckToSlot } from '@fortawesome/free-solid-svg-icons'
 const SingleMeal = ( props) => {
     const {meal} = props;
     const handleCheckList = props.handleCheckList;
@@ -10,7 +12,7 @@ const SingleMeal = ( props) => {
             <p>Area: {meal.strArea}</p>
             <p>Ingredients: {meal.strTags}</p>
             {/* <p>Instructions: {meal.strInstructions}</p> */}
-            <button onClick={()=> handleCheckList(meal)}>Add to CheckList</button>
+            <button onClick={()=> handleCheckList(meal)}>Add to CheckList <FontAwesomeIcon icon={faCheckToSlot} /></button>
         </div>
     );
 };
