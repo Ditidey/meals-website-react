a346004fbd8cb401dd1019a4a497aceff49922d3
Ditidey <ditidey2017@gmail.com>
2023-03-29 13:42:47 +0100

    cart updated

--- src/components/Carts/Cart.jsx
+++ src/components/Carts/Cart.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import './Cart.css'
 const Cart = ({checkList}) => {
-   console.log(checkList)
+//    console.log(checkList)
    let name;
    let img;
+   let inst;
+   const handleBack = () =>{
+      if(checkList.length > 0){
+        let ind = checkList.length - 1;
+ 
+      }
+      else{return alert('Select more')}
+       
+   }
    for(const id of checkList){
+     
     name = (id.strMeal)
     img = id.strMealThumb;
+    inst = id.strInstructions;
    }
     return (
         <div className='cart'>
            <h1>Check List</h1> 
            <p>Selected items: {checkList.length} </p>
-           
+             <button onClick={handleBack}>Back</button>
+             <button>Next</button>
            <div className='div'>
             <p>Item Name: {name}</p>
             <img className='img' src={img} alt="" />
+            <p>{inst}</p>
            </div>
         </div>
     );
